aba6ac231b378ab3bddac4cb0314051e60273343
Ravi kumar <1002kumarravi@gmail.com>
2022-04-03 12:02:21 +0530

    install: react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./pages/home/home";
+import ProductsPage from "./pages/products/products-combine-page";
+import WishlistPage from "./pages/wishlist/wishlsit";
+import SignUpPage from "./pages/signup/signup";
+import CartPage from "./pages/cart/cart";
+import Mockman from "mockman-js";
+import { ErrorPage } from "./pages/error-page/error-page";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/productspage" element={<ProductsPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/mockman" element={<Mockman />} />
+        <Route path = "*" element = {<ErrorPage/>}/>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { TokenProvider } from "./context/auth/token-context";
 import { makeServer } from "./server";
+import { BrowserRouter as Router } from "react-router-dom";
+import { SignUpProvider } from "./context/signup-context/signup-context";
+import { ProductsListProider } from "./context/products-context/products-list-context";
+import { UserProductsDataProvider } from "./service/getUserProductsData";
+import { ProductsDataProvider } from "./context/products-context/products-data";
+
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <TokenProvider>
+        <SignUpProvider>
+          <ProductsListProider>
+            <ProductsDataProvider>
+              <UserProductsDataProvider>
+              <App />
+              </UserProductsDataProvider>
+            </ProductsDataProvider>
+          </ProductsListProider>
+        </SignUpProvider>
+      </TokenProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
