facf137b4f0412d00c9010ca8757332e44652aa0
Ravi kumar <1002kumarravi@gmail.com>
2022-04-06 15:27:09 +0530

    feat: hmoe page categories, redirected to the product list page

--- src/pages/home/overlaycard.jsx
+++ src/pages/home/overlaycard.jsx
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom"
-
+import { productsDataContext } from "../../context/products-context/products-data"
+import { useContext } from "react";
 const OverlayCard = ({ overlayCardData }) => {
+
+    const { filteredApplyItemState, filteredApplyItemDispatch } = useContext(productsDataContext);
 
     return (
         <div className="home-overlay-card-grid home-overlay-card">
             {overlayCardData.map((item) => {
                 return (
                     <div className="image-card-container card-overlay-container image-card" key={item._id} >
-                        <Link to="/ProductsPage">
+                        <Link to="/productspage"
+                            onClick={
+                                (e) => {
+                                    const selectedItems = [...filteredApplyItemState.activeCategory, item.categoryName.toLowerCase()]
+                                    filteredApplyItemDispatch({
+                                        type: "CATEGORY",
+                                        payload: {
+                                            category: selectedItems,
+                                        },
+                                    });
+                                }
+                            }>
                             <div className="image-card-container">
                                 <img src={item.imgsources} className="image-card" />
                             </div>
