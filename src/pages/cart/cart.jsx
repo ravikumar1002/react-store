2ed92dcb46b6a9144147871f41bd49394764923c
Ravi kumar <1002kumarravi@gmail.com>
2022-04-06 15:30:03 +0530

    style: empty cart

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -3,6 +3,8 @@ import CartProductCard from "./cart-products-card"
 import "./cart.css"
 import { useContext } from "react";
 import { userProductsDataContext } from "../../service/getUserProductsData";
+import CartEmptyImg from "../../assets/empty-bag.png"
+import { EmptyPage } from "../../components/empty-page/EmptyPage";
 
 const CartPage = () => {
 
@@ -10,20 +12,27 @@ const CartPage = () => {
         userProductsDataContext
     );
 
+    const emptyWIshlist  = {
+        imgSrc : CartEmptyImg,
+        imgAlt: "Cart is empty",
+        emptyPageHeading: "Unfortunately, Your Cart is Empty",
+        emptyPageText: "Please Add Something in your Cart",
+    }
+
     return (
         <main>
-            <h2 className="text-center py-2 ">My Cart</h2>
-            <div className="centre gap-2">
-                <div>
+            <h2 className="text-center py-2 cart-heading">My Cart</h2>
+            <div className="cart-page-content gap-2">
+                <div className="cart-item-wrapper">
                     {userSavedProductsState?.cart.length > 0 && <CartProductCard selectedItemsForCart={userSavedProductsState.cart} />}
                 </div>
-                <div>
+                <div className="cart-price-wrapper">
                     {userSavedProductsState?.cart.length > 0 && <CartFinalPriceCard />}
                 </div>
                 <div>
                    {userSavedProductsState.cart.length === 0 && <div className="text-center mt-4">
-                       <p>Cart Page is Empty</p>
-                       </div>}  
+                   <EmptyPage  emptyPageData= {emptyWIshlist}/>
+                    </div>}  
                 </div>
             </div>
         </main>
