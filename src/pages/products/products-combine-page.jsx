9feb9b4863bd2da14e2ab0a82e5397250ebc6ba6
Ravi kumar <1002kumarravi@gmail.com>
2022-04-07 00:58:02 +0530

    fix: products page title

--- src/pages/products/products-combine-page.jsx
+++ src/pages/products/products-combine-page.jsx
@@ -1,8 +1,14 @@
 import Filter from "../../components/filter/Filter";
 import ProductsListShow from "./productslist";
 import "./products.css";
+import { useDocumentTitle } from "../../hooks/document-title";
+import { useEffect } from "react";
 
 const ProductsPageCombine = () => {
+    useEffect(() => {
+        useDocumentTitle("Products listing")
+    }, [])
+    
     return (
         <main className="d-flex gap-2  products-page-responsive">
             <Filter />
